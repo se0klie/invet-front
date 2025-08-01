be7f7d458e3277fe0c8261e81cac0597688ad3cd
hailie <142462676+se0klie@users.noreply.github.com>
2025-07-31 20:41:11 -0500

    new modals for payment, loading, cancel or so. added function to buttons.

--- src/pages/shared components/SubsBox.jsx
+++ src/pages/shared components/SubsBox.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { CancelButton } from "../../pages/shared components/Buttons";
 import { Box, Button, Typography } from "@mui/material";
 import { useState } from "react";
-
+import { CancelPlanModal, LoadingModal } from "./Modals";
 export default function SubsBox({ petName, date, plan }) {
     const isMobile = window.innerWidth <= 600;
     const [price, setPrice] = useState('$11.00');
@@ -15,10 +15,24 @@ export default function SubsBox({ petName, date, plan }) {
             setPrice('$24.15');
         }
     }, []);
+    const [cancelPlan, setCancelPlan] = useState(false)
+    const [loadingModal, setLoadingModal] = useState()
+    const [loadingModalStep, setLoadingModalStep] = useState(0)
+
+    function onCancelPlan() {
+        setCancelPlan(false)
+        setLoadingModal(true)
+        setTimeout(() => {
+            setLoadingModalStep(1)
+            setTimeout(() => {
+                setLoadingModal(false)
+            }, 2000);
+        }, 3000);
+    }
 
     return (
         <Box
-            sx={{ marginY: '1rem', display: 'flex', flexDirection: 'column', gap: '10px', backgroundColor: 'white', padding: '1rem', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',}}>
+            sx={{ marginY: '1rem', display: 'flex', flexDirection: 'column', gap: '10px', backgroundColor: 'white', padding: '1rem', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', }}>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: isMobile ? '1.5rem' : '1rem' }}>
                 <Typography variant="h6" sx={{ color: 'black', fontWeight: 'bold' }}>
                     {petName}
@@ -35,7 +49,7 @@ export default function SubsBox({ petName, date, plan }) {
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', gap: '1rem' }}>
 
                 <Box sx={{ width: '50%' }}>
-                    <CancelButton action={() => console.log('Cancelar suscripción')} text={`${isMobile ? 'Cancelar' : 'Cancelar plan'}`} />
+                    <CancelButton action={() => setCancelPlan(true)} text={`${isMobile ? 'Cancelar' : 'Cancelar plan'}`} />
                 </Box>
                 <Box sx={{ textAlign: 'left' }}>
                     <Typography variant="body2" sx={{ fontWeight: 'bold', color: '#007780' }}>
@@ -46,7 +60,6 @@ export default function SubsBox({ petName, date, plan }) {
                     </Typography>
                 </Box>
 
-                {/* Plan */}
                 <Box sx={{ textAlign: 'left' }}>
                     <Typography variant="body2" sx={{ fontWeight: 'bold', color: '#007780' }}>
                         Plan:
@@ -55,8 +68,15 @@ export default function SubsBox({ petName, date, plan }) {
                         {plan}
                     </Typography>
                 </Box>
-
             </Box>
+            <CancelPlanModal
+                open={cancelPlan}
+                setOpen={setCancelPlan}
+                petName={petName}
+                onCancel={onCancelPlan}
+            />
+
+            <LoadingModal open={loadingModal} setOpen={setLoadingModal} text="Generando cambios..." modalStep={loadingModalStep} />
         </Box>
     )
 }
