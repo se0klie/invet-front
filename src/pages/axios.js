21800539db01126df5d1c75dd88b9efdbb45539e
jojusuar <jojusuar@espol.edu.ec>
2025-10-15 23:57:53 -0500

    xd

--- src/pages/axios.js
+++ src/pages/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axios_api = axios.create({
-  baseURL: "https://backendinvet.com/api/",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}api`,
   headers: {
     "Content-Type": "application/json",
   },
